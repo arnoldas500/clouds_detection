521ab25fb275efb0a3eb297cfbdb57f0d846555a
Arnoldas Kurbanovas <akurbanovas@albany.edu>
2020-09-16 22:53:54 +0000

    update gitignore, image seq fixed for vid, obj dir started

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,27 @@
+# import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=32,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+# define the lower and upper boundaries of the "green"
+# ball in the HSV color space
+greenLower = (29, 86, 6)
+greenUpper = (64, 255, 255)
+# initialize the list of tracked points, the frame counter,
+# and the coordinate deltas
+pts = deque(maxlen=args["buffer"])
+
+print(args["buffer"])
+
+print(pts)
